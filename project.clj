a4944da76cbd14d44ca58c2919e3e741212a0bb7
Cark <phlex@telenet.be>
2012-09-19 07:20:36 +0200

    added support for lein-set-version
    Signed-off-by: Cark <phlex@telenet.be>

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/algo.monads "0.1.3-SNAPSHOT"]]
   :profiles {:dev {:plugins [[codox "0.6.1"]
-                             [lein-midje "2.0.0-SNAPSHOT"]]
+                             [lein-midje "2.0.0-SNAPSHOT"]
+                             [lein-set-version "0.2.1"]]
                    :dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]
                                   [midje "1.4.0"]]}}
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
-		 "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"})
+		 "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"}
+  :set-version {:updates [{:path "README.md"
+                           :search-regex #"cark/data.lenses \"\d+\.\d+\.\d+(-SNAPSHOT)?\""}]})
